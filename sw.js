02f316a2c4e33b0946a67134e777d75aaa9405d8
Taras Darchuk <taras.darchuk@gmail.com>
2024-01-27 22:25:06 +0100

    fix

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
 
 self.addEventListener("push", async (event) => {
   console.log("🚀 ~ self.addEventListener ~ event:", event)
-  const title = await event.data.json();
+  const title = await event.data.text();
   console.log("🚀 ~ self.addEventListener ~ body:", body)
   console.log("🚀 ~ self.addEventListener ~ title:", title)
   self.registration.showNotification(title, {
