0a7894859cb5a0cd7290a0f76829dfa687529f37
Taras Darchuk <taras.darchuk@gmail.com>
2024-01-27 21:53:13 +0100

    upf

--- sw.js
+++ sw.js
@@ -1,6 +1,13 @@
 
 
-self.addEventListener('push', (event) => {
-  console.log("🚀 ~ self.addEventListener ~ event:", event)
-  self.registration.showNotification('Hello world!', options);
+// self.addEventListener('push', (event) => {
+//   console.log("🚀 ~ self.addEventListener ~ event:", event)
+//   self.registration.showNotification('Hello world!', options);
+// });
+
+self.addEventListener("push", async (event) => {
+  const { title, body } = await event.data.json();
+  self.registration.showNotification(title, {
+    body,
+  });
 });
