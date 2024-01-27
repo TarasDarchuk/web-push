a881c192e5366cef94f2c110c0e36c762690109f
Taras Darchuk <taras.darchuk@gmail.com>
2024-01-27 22:10:19 +0100

    4el

--- sw.js
+++ sw.js
@@ -10,6 +10,7 @@ self.addEventListener("push", async (event) => {
   // const { title, body } = await event.data;
   // console.log("🚀 ~ self.addEventListener ~ body:", body)
   // console.log("🚀 ~ self.addEventListener ~ title:", title)
-  self.registration.showNotification(event.data.text(), {
+  self.registration.showNotification('Hello 4el', {
+  // self.registration.showNotification(event.data.text(), {
   });
 });
